6684f7292526bc6af4f1c72b91ba5ce6a1a83616
joanneks <joannekan.sh@gmail.com>
2022-07-28 11:15:36 +0800

    added update delete routs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
 const wax = require('wax-on');
+var helpers = require('handlebars-helpers')({
+    handlebars: hbs.handlebars
+  });
 
 const app = express();
 app.set('view engine','hbs');
